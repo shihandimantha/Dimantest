87207682b5267ccdac3105e26808757e7c3447e9
shihandimantha <76395781+shihandimantha@users.noreply.github.com>
2022-07-24 21:29:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
-global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.owner = ['94710697143']
+global.premium = ['94710697143']
+global.ownernomer = '94710697143'
+global.ownername = 'Diman'
+global.botname = 'Diman-Beta'
+global.footer = '©Diman.'
+global.ig = 'https://github.com/shihandimantha'
+global.region = 'Sri Lanka, Gampaha , Daluwakotuwa'
+global.sc = 'https://github.com/shihandimantha/Dimantest'
+global.myweb = 'https://kakuluwa.website/'
+global.packname = 'Diman-Beta'
+global.author = 'Diman'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
